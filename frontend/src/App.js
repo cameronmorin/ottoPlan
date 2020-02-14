318194671adb08f8ede543107f48cace5d46265c
Cameron <cmori007@ucr.edu>
2020-02-14 15:36:11 -0800

    Create initial auth flow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import firebase from 'firebase';
+import firebaseConfig from './service/fconfig';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccess: () => false
+  }
+};
+
+
+export default class App extends React.Component {
+  state = {
+    isAuthenticated: false,
+    currentUser: null
+  };
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({
+        isAuthenticated: !!user
+      });
+      
+      // Update state user object
+      if (user) {
+        this.setState({currentUser: user.providerData[0]});
+      }
+      else {
+        this.setState({currentUser: null});
+      }
+    });
+    console.log(this.state.currentUser);
+  }
+
+  render() {
+    return (
+      <div className="App-header">
+        {this.state.isAuthenticated ? 
+          <>
+            <img alt="profile picture" src={firebase.auth().currentUser.photoURL} style={{height: "75px", width: "75px", borderRadius: "50%"}}/>
+            <p>Hello, {firebase.auth().currentUser.displayName}!</p>
+            <button onClick={() => firebase.auth().signOut()}>Sign Out!</button> 
+          </>
+          : 
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        }
+      </div>
+    );
+  }
 }
-
-export default App;
--- frontend/src/service/firebase.js
+++ frontend/src/service/firebase.js
@@ -1,7 +1,28 @@
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import firebaseConfig from './fconfig';
+// import * as firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+// import firebaseConfig from './fconfig';
 
-console.log(firebaseConfig);
-// firebase.initializeApp(firebaseConfig);
+
+// class Firebase {
+//   constructor() {
+//     firebase.initializeApp(firebaseConfig);
+
+//     // Helper
+//     this.fieldValue = firebase.firestore.FieldValue;
+
+//     this.auth = firebase.auth();
+//     this.db = fireabse.firestore();
+  
+//     this.storage = firebase.storage();
+//     this.storageRef = this.storage.ref();
+
+//     // Google Auth Provider
+//     this.googleProvider = new firebase.auth.GoogleAuthProvider();
+//   }
+
+//   signInWithGoogle = () => {
+//     this.auth.signInWithPopup(this.googleProvider);
+//   }
+
+// }
