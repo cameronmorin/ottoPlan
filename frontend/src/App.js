9ab6290a899aa98f65643d3a63029258db7d59b7
Cameron <cmori007@ucr.edu>
2020-02-29 19:23:56 -0800

    Move sidebar items and refactor css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './style/App.css';
-
 import firebase from 'firebase';
-import firebaseConfig from './service/fconfig';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import { GoogleLogin } from 'react-google-login';
+import './style/App.css';
 import backend from './service/firebase';
 
-import Sidebar from './Sidebar';
-import HomeIcon from '@material-ui/icons/Home';
-import SettingsIcon from '@material-ui/icons/Settings';
-import FriendIcon from '@material-ui/icons/People';
-import AccountIcon from '@material-ui/icons/AccountBox';
-import SignoutIcon from '@material-ui/icons/ExitToApp';
-import EventIcon from '@material-ui/icons/Event';
-import NotifIcon from '@material-ui/icons/Notifications';
-import UnreadIcon from '@material-ui/icons/NotificationImportant';
-
-const uiConfig = {
-  signInFlow: 'popup',
-  signInOptions: [
-    {
-      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      scopes: [
-        'https://www.googleapis.com/auth/calendar',
-        'https://www.googleapis.com/auth/calendar.events',
-        'https://www.googleapis.com/auth/calendar.readonly'
-      ]
-    }
-  ],
-  callbacks: {
-    signInSuccess: () => false
-  }
-};
-
-const items = [
-  { name: 'home', label: 'Home', Icon: HomeIcon},
-  { name: 'friends', label: 'Friends', Icon: FriendIcon},
-  { name: 'create event', label: 'Create Event', Icon: EventIcon},
-  { name: 'notifications', label: 'Notifications', Icon: unread ? UnreadIcon : NotifIcon},
-  { 
-    name: 'settings', 
-    label: 'Settings',
-    Icon: SettingsIcon,
-    items: [
-             {name: 'account', label: 'Account', Icon: AccountIcon},
-             {name: 'sign out', label: 'Sign Out', Icon: SignoutIcon},
-           ],
-  },
-]
-
-var unread = false;
-
+import Sidebar from './components/Sidebar';
+import { Button } from 'react-bootstrap';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -85,7 +37,7 @@ export default class App extends React.Component {
   }
 
   popupSignIn = () => {
-    var GoogleProvider = new firebase.auth.GoogleAuthProvider;
+    var GoogleProvider = new firebase.auth.GoogleAuthProvider();
     GoogleProvider.addScope('https://www.googleapis.com/auth/calendar.events');
     GoogleProvider.addScope('https://www.googleapis.com/auth/calendar.readonly');
     firebase.auth().signInWithPopup(GoogleProvider).then(result => {
@@ -99,22 +51,24 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <>
-      <div className="Menu">
-        {this.state.isAuthenticated ? <Sidebar items={items}/> : null}
-      </div>
-      <div className="App-header">
-        {this.state.isAuthenticated ? 
-          <>
-            <img alt="profile picture" src={firebase.auth().currentUser.photoURL} style={{ height: "50px", width: "50px", borderRadius: "50%", position: "absolute", left: "10px", top: "15px"}}/>
-            <p>Hello, {firebase.auth().currentUser.displayName}!</p>
-            {/* <button onClick={() => firebase.auth().signOut()}>Sign Out!</button>  */}
-          </>
+      <div className='App'>
+        {this.state.isAuthenticated ?
+          <div className='home-grid'>
+            <div className='hg-left'>
+              <Sidebar photo={this.state.currentUser.photoURL}/>
+            </div>
+            <div className='hg-right'>
+              <p className='home-quip'>dashboard</p>
+            </div>
+          </div>
           :
-          <button onClick={this.popupSignIn}>Login Test</button>
+          <div className='home-login'>
+            <h1 className='home-title font-effect-3d-float'>ottoPlan</h1>
+            <p className='home-quip'>Reducing decision fatigue!</p>
+            <Button onClick={this.popupSignIn} variant='outline-light' className='home-btn'>Login with Google</Button>
+          </div>
         }
       </div>
-      </>
     );
   }
 }
---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,139 @@
+import React from "react";
+import firebase from "firebase";
+import "../style/sidebar.css";
+
+// Import Material Components
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import Collapse from "@material-ui/core/Collapse";
+// import ListItemText from "@material-ui/core/ListItemText";
+
+// Import Material Icons
+import HomeIcon from '@material-ui/icons/Home';
+import SettingsIcon from '@material-ui/icons/Settings';
+import FriendIcon from '@material-ui/icons/People';
+import AccountIcon from '@material-ui/icons/AccountBox';
+import SignoutIcon from '@material-ui/icons/ExitToApp';
+import EventIcon from '@material-ui/icons/Event';
+import NotifIcon from '@material-ui/icons/Notifications';
+import UnreadIcon from '@material-ui/icons/NotificationImportant';
+
+
+var unread = false;
+const items = [
+  { name: 'home', label: 'Home', Icon: HomeIcon},
+  { name: 'friends', label: 'Friends', Icon: FriendIcon},
+  { name: 'create event', label: 'Create Event', Icon: EventIcon},
+  { name: 'notifications', label: 'Notifications', Icon: unread ? UnreadIcon : NotifIcon},
+  { 
+    name: 'settings', 
+    label: 'Settings',
+    Icon: SettingsIcon,
+    items: [
+             {name: 'account', label: 'Account', Icon: AccountIcon},
+             {name: 'sign out', label: 'Sign Out', Icon: SignoutIcon},
+           ],
+  },
+]
+
+
+function SidebarItem({ depthStep = 10, depth = 0, expanded, item, ...rest }) {
+  const [collapsed, setCollapsed] = React.useState(true);
+  const { label, items, Icon, onClick: onClickProp } = item;
+
+  function toggleClose() {
+    setCollapsed(previous => !previous);
+  }
+
+  function onClick(e) {
+    if (item.label === "Sign Out") {
+      firebase.auth().signOut();
+    }
+    if (Array.isArray(items)) {
+      toggleClose();
+    }
+    if (onClickProp) {
+      onClickProp(e, item);
+    }
+  }
+
+  let expandedIcon;
+
+  if (Array.isArray(items) && items.length) {
+    expandedIcon = !collapsed ? (
+      <ExpandLessIcon className={"expand-arrow"} />
+    ) : (
+      <ExpandMoreIcon className={"expand-arrow"} />
+    );
+  }
+
+  return (
+    <>
+      <ListItem
+        className="sidebar-item"
+        onClick={onClick}
+        button
+        dense
+        {...rest}
+      >
+        <div
+          style={{ paddingLeft: depth * depthStep }}
+          className="sidebar-item-stuff"
+        >
+          {Icon && <Icon className="sidebar-item-icon" />}
+          <div className="sidebar-item-text">{label}</div>
+        </div>
+        {expandedIcon}
+      </ListItem>
+
+      <Collapse in={!collapsed} timeout="auto" unmountOnExit>
+        {Array.isArray(items) ? (
+          <List disablePadding dense>
+            {items.map((subItem, index) => (
+              <React.Fragment key={`${subItem.name}${index}`}>
+                {subItem === "divider" ? (
+                  <Divider style={{ margin: "10px 0" }} />
+                ) : (
+                  <SidebarItem
+                    depth={depth + 1}
+                    depthStep={depthStep}
+                    item={subItem}
+                  />
+                )}
+              </React.Fragment>
+            ))}
+          </List>
+        ) : null}
+      </Collapse>
+    </>
+  );
+}
+
+function Sidebar({ photo, depthStep, depth, expanded }) {
+  return (
+    <div className="sidebar">
+      <List disablePadding dense>
+        <img alt='profile pic' src={photo} className='sidebar-item profile-pic' />
+        {items.map((sidebarItem, index) => (
+          <React.Fragment key={`${sidebarItem.name}${index}`}>
+            {sidebarItem === "divider" ? (
+              <Divider style={{ margin: "6px 0" }} />
+            ) : (
+              <SidebarItem
+                depth={depth}
+                depthStep={depthStep}
+                expanded={expanded}
+                item={sidebarItem}
+              />
+            )}
+          </React.Fragment>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default Sidebar;
