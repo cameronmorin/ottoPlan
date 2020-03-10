d80714f964fb926f54ff62ab84da664a22ebbc2d
padawong <erin.b.wong@gmail.com>
2020-03-10 16:39:54 -0700

    Integrating attendee info/tokens

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ const TOKEN_PATH = 'token.json';
  */
 // #1 in the async daisy-chain
 // Tried and failed to return oAuth2Client to calling function to send directly to subsequent functions
-async function authorize(credentials, token, callback, request_data, all_busy) {
+async function authorize(credentials, tokens, request_data, all_busy) {
     console.log('authorize begin\n');
     const {client_secret, client_id, redirect_uris} = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
@@ -47,7 +47,7 @@ async function authorize(credentials, token, callback, request_data, all_busy) {
         // rmail refresh token
         console.log('setting credentials');
         //var token_obj = {refresh_token: token};
-        oAuth2Client.setCredentials({refresh_token: token});
+        oAuth2Client.setCredentials(JSON.parse(tokens));
         callback(oAuth2Client, request_data, all_busy)
             .then(response => {
                 resolve(response);
@@ -176,7 +176,6 @@ async function scheduleEvent(request_data, credentials) {
 
     //TODO: after attendees come in from frontend correctly, call workingHours from here instead of getAllBusy
     return new Promise( (resolve, reject) => {
-            // TODO: adjust this for any number of attendees from request_data
             //console.log('scheduleEvent Promise begin\n');
         /*
         var attendees = [];
@@ -190,12 +189,12 @@ async function scheduleEvent(request_data, credentials) {
         //console.log('attendees[0]: ' + attendees[0] + '\n');
         //console.log('attendees[1]: ' + attendees[1] + '\n');
 
-        /*
         // Initialize all_busy with non-working hours/days
         //console.log('Calling workingHours\n');
         var all_busy = [];
         all_busy = workingHours(request_data);
         //console.log('scheduleEvent all_busy after working hours: ' + JSON.stringify(all_busy, null, 2) + '\n');
+        /*
 
         var i;
         // Populate all_busy using calendar info from all attendees
@@ -239,8 +238,13 @@ async function scheduleEvent(request_data, credentials) {
                     //request_data.event_end = all_busy[1].start;
                     var attendees = [];
                     // gmail refresh token
-                    attendees.push('1//06ue5SnV-Ua2JCgYIARAAGAYSNwF-L9IrIbsANAwzjCyFRctMpX4eQnfWLpTRmH-3h6fu4nM0-nnRhVYvFzReBQ-hc-DeLp5a9d0');
-                    //attendees.push('AEu4IL0eJknM-ODSU0w36d4CdgB4-DA3oXom5FpaTqssIww-j4LF2ASw92mTP9JyO51uQ277DtMkc1NYakXk9PKPbXJCSzQ81FngKOiH7mXC34bO6DB45BXZXX80Dsnk_xn781xpmH62k8rlmmGEHgGa-apo8JxlJ-FUmTU0Q5y0fm6VZv5ygDwBaJoWwpykCjfj0GyFRm4OFILQ-JfZBCMiq266dEDYu2Fly-lrAtR4A2uyY4zITT-_891LNs-HzWexSF3feJlBTGnVWpzdmY8MUe51Y2Y0ugZeZtiyDaEu7cUx-dlR0QiALLvnjwuBBQIw9pGOwmkYHwwcgyOHjotOKmyBJDrUYgOgNqsVLl-BacRg6NWGToNTwXT_PFk5fmIfDIy7ltE8ZjmlayGe3zlkC1BxbKKnutiHivDqAEbg7tCIZU0X0yq4ZnmIpaxcDNVy1ywS_WwK');
+                    //attendees.push('1//06ue5SnV-Ua2JCgYIARAAGAYSNwF-L9IrIbsANAwzjCyFRctMpX4eQnfWLpTRmH-3h6fu4nM0-nnRhVYvFzReBQ-hc-DeLp5a9d0');
+                    //attendees.push('AEu4IL1OhPZpdF6PQ6TtaG8rm3ZLnv_7r6IuJGOXK8tAtU5PRcxVvsywMr1LisWqnhjITwprYVGE8nM3VhIAtj2pxLuARogddcIrbzhPJ29PefYU6nbUX6DEnjI9-0kwPgQUiEND8Qm6qMZ-z2CXTpk2df66NL2VUFUW7zZyroAp5BXtv-VEcgvAvFdQkudFPoyoC_aNHcEPAzqPzVyPuqlKJhZC2L34Dke008HJLp-oXhs75j7czf9-1iAKhOmChfHCU9D7b4r3csBoYMhMTKDFuCP3Ips6Wuv8B4as3PumUs9fPuSfUSF6jdwMboACMjxTJ_78fjIK_ueHY0mrSySnKkfgAEM9GVaXIWzRYir6HjfxVsE67j7S1WjC02S5xA-h6bIhOJpzsA6bEkzkse9WBXgoa_xpFyeZX5I1ZJCaCt3xXSQJn11EKLwaIdcIdcamPgatUlZd');
+                    /*
+                    var obj = {refresh: 'AEu3IL1OhPZpdF6PQ6TtaG8rm3ZLnv_7r6IuJGOXK8tAtU5PRcxVvsywMr1LisWqnhjITwprYVGE8nM3VhIAtj2pxLuARogddcIrbzhPJ29PefYU6nbUX6DEnjI9-0kwPgQUiEND8Qm6qMZ-z2CXTpk2df66NL2VUFUW7zZyroAp5BXtv-VEcgvAvFdQkudFPoyoC_aNHcEPAzqPzVyPuqlKJhZC2L34Dke008HJLp-oXhs75j7czf9-1iAKhOmChfHCU9D7b4r3csBoYMhMTKDFuCP3Ips6Wuv8B4as3PumUs9fPuSfUSF6jdwMboACMjxTJ_78fjIK_ueHY0mrSySnKkfgAEM9GVaXIWzRYir6HjfxVsE67j7S1WjC02S5xA-h6bIhOJpzsA6bEkzkse9WBXgoa_xpFyeZX5I1ZJCaCt3xXSQJn11EKLwaIdcIdcamPgatUlZd', access: 'ya29.a0Adw1xeUvRhny--RXO6FwZM1rB0gzLqG5r-lcfBpzeLUaD655NOnW5swe7RvllokRagw0BznTmUM_vNUQbNi-FvMG2bibSDLnyJEkwLMdPcwIcEQTykfCV6bf96r6NtXw_UowZjlEu6ITO4hb45n7Bxr2fQW7MY2qejCsbv7k51MjnOig'}
+                    attendees.push(obj);
+                    */
+                    //attendees.push('AEu3IL0eJknM-ODSU0w36d4CdgB4-DA3oXom5FpaTqssIww-j4LF2ASw92mTP9JyO51uQ277DtMkc1NYakXk9PKPbXJCSzQ81FngKOiH7mXC34bO6DB45BXZXX80Dsnk_xn781xpmH62k8rlmmGEHgGa-apo8JxlJ-FUmTU0Q5y0fm6VZv5ygDwBaJoWwpykCjfj0GyFRm4OFILQ-JfZBCMiq266dEDYu2Fly-lrAtR4A2uyY4zITT-_891LNs-HzWexSF3feJlBTGnVWpzdmY8MUe51Y2Y0ugZeZtiyDaEu7cUx-dlR0QiALLvnjwuBBQIw9pGOwmkYHwwcgyOHjotOKmyBJDrUYgOgNqsVLl-BacRg6NWGToNTwXT_PFk5fmIfDIy7ltE8ZjmlayGe3zlkC1BxbKKnutiHivDqAEbg7tCIZU0X0yq4ZnmIpaxcDNVy1ywS_WwK');
                     //attendees.push(request_data.gmail_refresh_token);
 
                     console.log('Calling findWindow\n');
@@ -248,7 +252,7 @@ async function scheduleEvent(request_data, credentials) {
                     //TODO: figure out how to have it use the right auth info to create the event
                     // have attendees list include scheduler's refresh token
                     //TODO: implement scheduler_token
-                    authorize(credentials, attendees[0], findWindow, request_data, all_busy)
+                    authorize(credentials, request_data.schedule_info.organizer.tokens, findWindow, request_data, all_busy)
                     //findWindow(auth, all_busy, request_data)
                         .then(event => {
                             console.log('Scheduled event: ' + JSON.stringify(event));//, null, 2));
@@ -283,14 +287,16 @@ async function scheduleEvent(request_data, credentials) {
 
 async function getAllBusy(credentials, request_data) {
     return new Promise( (resolve, reject) => {
-        var attendees = [];
+        //var attendees = [];
         //attendees.push(request_data.gmail_refresh_token);
         //attendees.push(request_data.rmail_refresh_token);
         // TODO: remove this after attendees implemented in frontend
         // gmail token
-        attendees.push('1//06ue5SnV-Ua2JCgYIARAAGAYSNwF-L9IrIbsANAwzjCyFRctMpX4eQnfWLpTRmH-3h6fu4nM0-nnRhVYvFzReBQ-hc-DeLp5a9d0');
+        //attendees.push('1//06ue5SnV-Ua2JCgYIARAAGAYSNwF-L9IrIbsANAwzjCyFRctMpX4eQnfWLpTRmH-3h6fu4nM0-nnRhVYvFzReBQ-hc-DeLp5a9d0');
         // rmail token
-        attendees.push('1//06CQRedBAeU1bCgYIARAAGAYSNwF-L9Ir1jzPH6gzOViheTUTWrR_S4SYw_p61pv4yN3Ob1g3OKzn95y7Xx5t3sbmtWPdhT9ECGU');
+        //attendees.push('1//06CQRedBAeU1bCgYIARAAGAYSNwF-L9Ir1jzPH6gzOViheTUTWrR_S4SYw_p61pv4yN3Ob1g3OKzn95y7Xx5t3sbmtWPdhT9ECGU');
+        //var obj = {refresh: 'AEu4IL1OhPZpdF6PQ6TtaG8rm3ZLnv_7r6IuJGOXK8tAtU5PRcxVvsywMr1LisWqnhjITwprYVGE8nM3VhIAtj2pxLuARogddcIrbzhPJ29PefYU6nbUX6DEnjI9-0kwPgQUiEND8Qm6qMZ-z2CXTpk2df66NL2VUFUW7zZyroAp5BXtv-VEcgvAvFdQkudFPoyoC_aNHcEPAzqPzVyPuqlKJhZC2L34Dke008HJLp-oXhs75j7czf9-1iAKhOmChfHCU9D7b4r3csBoYMhMTKDFuCP3Ips6Wuv8B4as3PumUs9fPuSfUSF6jdwMboACMjxTJ_78fjIK_ueHY0mrSySnKkfgAEM9GVaXIWzRYir6HjfxVsE67j7S1WjC02S5xA-h6bIhOJpzsA6bEkzkse9WBXgoa_xpFyeZX5I1ZJCaCt3xXSQJn11EKLwaIdcIdcamPgatUlZd', access: 'ya29.a0Adw1xeUvRhny--RXO6FwZM1rB0gzLqG5r-lcfBpzeLUaD655NOnW5swe7RvllokRagw0BznTmUM_vNUQbNi-FvMG2bibSDLnyJEkwLMdPcwIcEQTykfCV6bf96r6NtXw_UowZjlEu6ITO4hb45n7Bxr2fQW7MY2qejCsbv7k51MjnOig'}
+        //attendees.push(obj);
         var all_busy = [];
         all_busy = workingHours(request_data);
         console.log('complete workingHours all_busy in getAll: ' + JSON.stringify(all_busy) + '\n');
@@ -301,10 +307,17 @@ async function getAllBusy(credentials, request_data) {
         (function next(i) {
             console.log('in function next; i = ' + i + '\n');
             if (i === attendees.length) {
-                resolve(all_busy);
+                authorize(credentials, request_data.schedule_info.organizer.tokens, getOwnCal, request_data, all_busy)
+                    .then(all_busy => {
+                        all_busy = JSON.parse(JSON.stringify(new_all_busy));
+                        resolve(all_busy);
+                    })
+                    .catch(err => {
+                        console.log('Failed to access busy info of organizer: ' + err);
+                    }
             }
 
-            authorize(credentials, attendees[i], getOwnCal, request_data, all_busy)
+            authorize(credentials, request_data.schedule_info.attendees[i].tokens, getOwnCal, request_data, all_busy)
             //TODO: implement attendees' refresh_token
                 .then(new_all_busy => {
                     all_busy = JSON.parse(JSON.stringify(new_all_busy));
@@ -569,6 +582,12 @@ function ISODateString(d){
 
 async function createEvent(auth, request_data) {
     console.log('Creating event using Calendar API...\n');
+    var attendee_emails = [];
+    var i;
+    for (i = 0; i < request_data.schedule_info.attendees.length; i++) {
+        attendee_emails.push(request_data.schedule_info.attendees.email);
+    }
+    console.log('attendee_emails: ' + JSON.stringify(attendee_emails, null, 2) + '\n');
 
     //console.log(own_events);
     const calendar = google.calendar({version: 'v3', auth});
@@ -595,10 +614,7 @@ async function createEvent(auth, request_data) {
         },
         // TODO: Importing emails from request_data isn't working right now because it's coming in as a list of email addresses, but we need it as an object in the format below
         //attendees: request_data.email,
-        attendees: [
-            { email: 'ewong012@ucr.edu'}, 
-            { email: 'dsiv001@ucr.edu' }
-        ],
+        attendees: attendee_emails,
         reminders: {
             useDefault: false,
             overrides: [
@@ -619,8 +635,8 @@ async function createEvent(auth, request_data) {
                 auth: auth,
                 //calendarId: otto_cal_ID,
                 // TODO: remove this and change back to primary when done with testing
-                calendarId: 'aa2ab10qanobloa2g9eqh7i50o@group.calendar.google.com',
-                //calendarId: 'primary',
+                //calendarId: 'aa2ab10qanobloa2g9eqh7i50o@group.calendar.google.com',
+                calendarId: 'primary',
                 resource: new_event,
                 sendNotifications: true,
             },
